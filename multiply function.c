3f80c849bc67844c69cbb51d88051cd6f4d5e931
AMF0 <109762993+AMF0@users.noreply.github.com>
2022-12-14 07:15:23 +0200

    Update multiply function.c

--- multiply function.c
+++ multiply function.c
@@ -1,11 +1,16 @@
+//re --> result
+//m matrix1
+//n matrix2
+// r1, r2 dimensions
 
-void multiply int(m[n][n], int n[n][n], re[n][n])
+void multiply int(m[r1][r2], int n[r1][r2], re[r1][r2])
 {
     for(int i = 0; i < n; i++)
     {
-        for(int j = 0; j < n; j++)
+        for(int j = 0; j < m; j++)
         {
             re[i][j] = 0;   //set initial value to zero 
+            
             for(int k = 0; k < n; k++)
                 result[i][j] += m[i][k] * n[k][j];
         }
