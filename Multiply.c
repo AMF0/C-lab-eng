f10c33f6ad356fe2829ec1d3ec68d281dd13227f
AMF0 <109762993+AMF0@users.noreply.github.com>
2022-12-15 09:51:01 +0200

    Update Multiply.c

--- Multiply.c
+++ Multiply.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-    int r1 = 0 , c1 = 0 ,r2 = 0, c2 = 0;
+    int r1 = 0 , c1 = 0 ,r2 = 0, c2 = 0, tmp = 0;
     float mat1[r1][c1];
     float mat2[r2][c2];
     float resMat[r1][c2];
@@ -10,24 +10,24 @@ int main(void)
     printf("\nMatrix 1 :: \n");
     printf("-------------\n\n");
 
-    printf("Please enter the number of rows : ");  //receive the number of rows
+    printf("Please enter the number of rows : ");  //receive the number of rows for first matrix
     scanf("%d", &r1);
 
-    printf("Please enter the number of columns : ");  //receive the number of columns
+    printf("Please enter the number of columns : ");  //receive the number of columns for first matrix
     scanf("%d", &c1);
 
     printf("\nMatrix 2 :: \n");
     printf("-------------\n\n");
 
-    printf("Please enter the number of rows : ");  //receive the number of rows
+    printf("Please enter the number of rows : ");  //receive the number of rows for second matrix
     scanf("%d", &r2);
 
-    printf("Please enter the number of columns : ");  //receive the number of columns
+    printf("Please enter the number of columns : ");  //receive the number of columns for second matrix
     scanf("%d", &c2);
 
-    if(c1 == r2)
+    if(c1 == r2)  //checking matrix multiplication conditions.
     {
-        printf("\nPlease enter the elements of matrix A\n");
+        printf("\nPlease enter the elements of matrix A\n");   //matrix a elements
         printf("----------------------------------------\n\n");
 
         for(int i = 0; i < r1; i++)
@@ -39,7 +39,7 @@ int main(void)
             }
         }
 
-        printf("\nPlease enter the elements of matrix B\n");
+        printf("\nPlease enter the elements of matrix B\n");  //matrix b elements
         printf("----------------------------------------\n\n");
 
         for(int i = 0; i < r2; i++)
@@ -50,16 +50,18 @@ int main(void)
                 scanf("%f",&mat2[i][j]);
             }
         }
-
+        //performing multiplication.
         for(int i = 0; i < r1; i++)
         {
             for(int j = 0; j < c2; j++)
             {
-                resMat[i][j]=0;
-                for(int k = 0; k < r2; k++)
+                tmp = 0;
+                for(int k = 0; k < c1; k++)
                 {
-                    resMat[i][j] += mat1[i][k] * mat2[k][j];
+                    tmp += mat1[i][k] * mat2[k][j];
                 }
+                resMat[i][j] = tmp;
+
             }
         }
 
@@ -68,7 +70,7 @@ int main(void)
         {
             for(int j = 0; j < c2; j++)
             {
-                printf("%.f\t",resMat[i][j]);
+                printf("%.f\t", resMat[i][j]);
             }
 
         printf("\n");
